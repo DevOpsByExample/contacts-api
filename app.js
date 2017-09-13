d5eaa49764d81e69baff7d16a8a385ab8094ed0f
Arunvel Sriram <arunvelsriram@gmail.com>
2017-09-14 00:18:59 +0530

    Make error handler respond JSON instead of HTML

--- app.js
+++ app.js
@@ -46,13 +46,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.error(err);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({message: err.message});
 });
 
 module.exports = app;
