d86c7e5bc1d7fa54ccbf895f80e2eef851a3ff02
Krishnaswamy Subramanian <jskswamy@gmail.com>
2017-09-20 19:28:30 +0530

    Use database host from environment

--- util/sequelize.js
+++ util/sequelize.js
@@ -1,7 +1,7 @@
 var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: 'localhost',
+  host: process.env.DB_HOST || 'localhost',
   dialect: 'postgres',
 
   pool: {
