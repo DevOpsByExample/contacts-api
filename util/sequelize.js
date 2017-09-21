36aa7b211c88161a2017426f7ba45efa2d45b2b0
Arunvel Sriram <arunvelsriram@gmail.com>
2017-09-21 23:28:12 +0530

    Write logs to file instead of console.log

--- util/sequelize.js
+++ util/sequelize.js
@@ -1,9 +1,18 @@
+var fs = require('fs');
+var path = require('path')
 var Sequelize = require('sequelize');
 const config = require(`../config/${process.env.NODE_ENV}.js`);
+
+const writeToFile = function(content) {
+	var logFile = fs.createWriteStream(path.join(__dirname, '../sequelize.log'), {flags: 'a'});
+	logFile.write(`${content}\n`);
+	logFile.end();
+}
 
 var sequelize = new Sequelize(config.dbName, config.dbUsername, config.dbPassword, {
   host: config.dbHostname,
   dialect: 'postgres',
+  logging: writeToFile,
 
   pool: {
     max: 5,
