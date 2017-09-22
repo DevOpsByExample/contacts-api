b949c1ff7cb0b70fb7b9b9bb230b93c5f541ccb8
Arunvel Sriram <arunvelsriram@gmail.com>
2017-09-22 19:54:52 +0530

    Remove unused code

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const favicon = require('serve-favicon');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -16,31 +15,18 @@ sequelize.authenticate()
   logger.error('Failed to establish connection with DB', err);
 });
 
-const index = require('./routes/index');
-const users = require('./routes/users');
 const contacts = require('./routes/contacts');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
 process.env.NODE_ENV === 'development' && app.use(morgan('dev'));
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'app.log'), {flags: 'a'})
 app.use(morgan('combined', {stream: accessLogStream}));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors());
 
-app.use('/', index);
-app.use('/users', users);
 app.use('/contacts', contacts);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/status', function(req, res, next) {
+	res.json({message: 'API works!'});
 });
 
 module.exports = router;
