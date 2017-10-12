594f2a7aae6c92b74f99fad1da2a0a19419bafda
Arunvel Sriram <arunvelsriram@gmail.com>
2017-10-12 18:26:41 +0530

    Revert "Add db host to prod config"
    This reverts commit 0d9fc4ab58f6900c01ed46dce6515d6d40c1c8b1.

--- config/production.js
+++ config/production.js
@@ -1,6 +1,5 @@
 module.exports = {
   dbName: process.env.DB_NAME,
   dbUsername: process.env.DB_USERNAME,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHostname: process.env.DB_HOST,
+  dbPassword: process.env.DB_PASSWORD
 };
